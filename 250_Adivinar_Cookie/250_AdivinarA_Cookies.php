bfd08ad580374dfbfa1e01661e67906c17517abd
AdrianCardin <acardinlozano@gmail.com>
2021-10-21 16:34:27 +0200

    Adivinar cookies

---
+++ 250_Adivinar_Cookie/250_AdivinarA_Cookies.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+
+<body>
+
+    <h1>ADIVINAR EL NÚMERO</h1>
+
+    <form action="./250_AdivinarB_Cookie.php" method="post">
+        <p>Jugador 1: Introduce un número:</p>
+        <input type="number" name="oculto" />
+        <input type="submit" value="Guardar oculto" />
+    </form>
+
+</body>
+
+</html>

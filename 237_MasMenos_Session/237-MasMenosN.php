e4e4f0937a143c547d6e7011c6b4ddec110e9d89
AdrianCardin <acardinlozano@gmail.com>
2021-10-25 13:21:48 +0200

    añadido mas menos sesion

--- 237_MasMenos_Session/237-MasMenosN.php
+++ 237_MasMenos_Session/237-MasMenosN.php
@@ -1,29 +1,27 @@
 <?php
-                                                    // server should keep session data for AT LEAST 1 hour
-                                                    //ini_set('session.gc_maxlifetime', 3600);
-                                                    session_set_cookie_params(3600);
-                                                    session_start(); // ready to go!
+        session_set_cookie_params(3600); // convierte los parametros de la sesion en cookies dentro de la sesion y no del navegador
+        session_start(); // se inicia la sesion
 
-if (isset($_SESSION["acumulado"]) && !(isset($_REQUEST["reset"]))) {
+        if (isset($_SESSION["acumulado"]) && !(isset($_REQUEST["reset"]))) {
 
-    $acumulado = $_SESSION["acumulado"];
-    isset($_REQUEST["diferencia"]) ? $diferencia = $_REQUEST["diferencia"] : $diferencia = 1;
+            $acumulado = $_SESSION["acumulado"];
+            isset($_REQUEST["diferencia"]) ? $diferencia = $_REQUEST["diferencia"] : $diferencia = 1;
 
-    if (isset($_REQUEST["resta"])) {
-        $acumulado = $acumulado - $diferencia;
-    } elseif (isset($_REQUEST["suma"])) {
-        $acumulado = $acumulado + $diferencia;
-    }
-} else {
-    // Si NO hay formulario enviado (1ª vez), o piden resetear.
-    $acumulado = 0;
-    isset($_REQUEST["diferencia"]) ? $diferencia = $_REQUEST["diferencia"] : $diferencia = 1;
-}
+            if (isset($_REQUEST["resta"])) {
+                $acumulado = $acumulado - $diferencia;
+            } elseif (isset($_REQUEST["suma"])) {
+                $acumulado = $acumulado + $diferencia;
+            }
+        } else {
+            // Si NO hay formulario enviado (1ª vez), o piden resetear.
+            $acumulado = 0;
+            isset($_REQUEST["diferencia"]) ? $diferencia = $_REQUEST["diferencia"] : $diferencia = 1;
+        }
 
-if (isset($_REQUEST["reset"])) {
-    $_SESSION["acumulado"] = 0;
-}
-$_SESSION["acumulado"] = $acumulado;
+        if (isset($_REQUEST["reset"])) {
+            $_SESSION["acumulado"] = 0;
+        }
+        $_SESSION["acumulado"] = $acumulado;
 ?>
 
 

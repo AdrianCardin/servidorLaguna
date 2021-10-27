4d4ec32085cabe6fc22ef6accdb246da072fad31
AdrianCardin <acardinlozano@gmail.com>
2021-10-27 16:23:58 +0200

    casi terminado ejercicio tablas con BBDD

---
+++ 300 Ejercicios/bbdd/index.php
@@ -0,0 +1,64 @@
+<?php
+
+    $servidor = "localhost";
+    $identificador = "root";
+    $contrasenna = "";
+    $bd = "agenda";
+
+    $opciones = [
+        PDO::ATTR_EMULATE_PREPARES   => false, // Modo emulación desactivado para prepared statements "reales"
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Que los errores salgan como excepciones
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // El modo de fetch que queremos por defecto.
+    ];
+
+    try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8", $identificador, $contrasenna, $opciones);
+    } catch (Exception $e) {
+        error_log("Error al conectar: " . $e->getMessage());
+        exit('Error al conectar');
+    }
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>Conexion a bbdd</title>
+</head>
+
+<body>
+    <?php
+    $sql = "SELECT * FROM Categoria ORDER BY Nombre";
+    $select = $conexion->prepare($sql); // se prepara la sql
+    $select->execute([]);
+    $resultado = $select->fetchAll();
+
+    ?>
+
+    <div class="tabla">
+        <h1>Tabla resultado</h1>
+        <table>
+            <tr>
+                <td>Nombre</td>
+            </tr>
+
+
+            <?php
+            //nombres de la obtencion de bbdd
+            foreach ($resultado as $fila) {
+                echo "<tr><td>" . $fila['nombre'] . "</td></tr>";
+            }
+            ?>
+
+        </table>
+    </div>
+</body>
+
+</html>
---
+++ 300 Ejercicios/tools.php
@@ -0,0 +1,26 @@
+<?php
+function conectarABBDD()
+{
+    $servidor = "localhost";
+    $identificador = "root";
+    $contrasenna = "";
+    $bd = "agenda";
+
+    $opciones = [
+        PDO::ATTR_EMULATE_PREPARES   => false, // Modo emulación desactivado para prepared statements "reales"
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Que los errores salgan como excepciones
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // El modo de fetch que queremos por defecto.
+    ];
+
+    try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=$bd;charset=utf8", $identificador, $contrasenna, $opciones);
+    } catch (Exception $e) {
+        error_log("Error al conectar: " . $e->getMessage());
+        exit('Error al conectar');
+    }
+}
+
+function sqlPredefinida()
+{
+    return "SELECT * FROM Categoria ORDER BY Nombre";
+}
